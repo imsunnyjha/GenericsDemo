5c5a83757e70ee3aa700682971be80ae9a56e93c
Sunny Jha <sunnyjha11897@gmail.com>
2020-10-20 09:18:40 +0530

    [SUNNY]Add. Implemeting generic class

---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5, 6 };
+            double[] doubleArray = { 1.1, 1.2, 1.3, 1.4 };
+            char[] charArray = { 'A', 'B', 'h', 'F' };
+
+            Program.ToPrint(intArray);
+            Program.ToPrint(doubleArray);
+            Program.ToPrint(charArray);
+        }
+        public static void ToPrint<TType>(TType[] inputArray)
+        {
+            foreach(var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("*********************");
+        }
+    }
+}
